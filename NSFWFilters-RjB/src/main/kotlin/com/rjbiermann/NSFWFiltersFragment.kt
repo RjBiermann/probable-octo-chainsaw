eb12c39ce7905c05fdbd090600d2911ba863de8c
Robert <RjBiermann@proton.me>
2025-04-18 20:44:21 -0400

    added ondestroy to save filters

--- NSFWFilters-RjB/src/main/kotlin/com/rjbiermann/NSFWFiltersFragment.kt
+++ NSFWFilters-RjB/src/main/kotlin/com/rjbiermann/NSFWFiltersFragment.kt
@@ -1,17 +1,18 @@
 package com.rjbiermann
 
 import android.content.SharedPreferences
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.EditText
 import android.widget.Switch
 import android.widget.TextView
 import androidx.annotation.RequiresApi
+import androidx.core.content.res.ResourcesCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.lagradost.cloudstream3.CommonActivity.showToast
 
@@ -19,6 +20,18 @@ class NSFWFiltersFragment(
     val plugin: TestPlugin, private val sharedPref: SharedPreferences
 ) : BottomSheetDialogFragment() {
     private val res = plugin.resources ?: throw Exception("Unable to read resources")
+    private lateinit var hdOnlySwitch: Switch
+    private lateinit var durationTextView: TextView
+    private lateinit var minDurationEditText: EditText
+    private lateinit var maxDurationEditText: EditText
+    private lateinit var addSearchHomeTextView: TextView
+    private lateinit var searchHomeEditText: EditText
+    private lateinit var searchHomeSortTextView: TextView
+    private lateinit var addSearchHomeSortEditText: EditText
+    private lateinit var searchSortHintTextView: TextView
+    private lateinit var disclaimerTextView: TextView
+
+    private var cleared = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +42,12 @@ class NSFWFiltersFragment(
             name, "string", BuildConfig.LIBRARY_PACKAGE_NAME
         )
         return res.getString(id)
+    }
+
+    private fun getDrawable(name: String): Drawable? {
+        val id =
+            plugin.resources!!.getIdentifier(name, "drawable", BuildConfig.LIBRARY_PACKAGE_NAME)
+        return ResourcesCompat.getDrawable(plugin.resources!!, id, null)
     }
 
     private fun <T : View> View.findView(name: String): T {
@@ -54,30 +73,26 @@ class NSFWFiltersFragment(
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // All the views
-        val hdOnlySwitch = view.findView<Switch>("hdOnly")
-        val durationTextView = view.findView<TextView>("duration")
-        val minDurationEditText = view.findView<EditText>("minDuration")
-        val maxDurationEditText = view.findView<EditText>("maxDuration")
-        val saveButton = view.findView<Button>("saveButton")
-        val addSearchHomeTextView = view.findView<TextView>("addSearchHome")
-        val searchHomeEditText = view.findView<EditText>("searchHomeEdit")
-        val searchHomeSortTextView = view.findView<TextView>("searchSort")
-        val addSearchHomeSortEditText = view.findView<EditText>("addSearchSort")
-        val clearButton = view.findView<Button>("clearButton")
-        val searchSortHintTextView = view.findView<TextView>("searchSortHint")
-        val disclaimerTextView = view.findView<TextView>("disclaimerText")
+        hdOnlySwitch = view.findView<Switch>("hdOnly")
+        durationTextView = view.findView<TextView>("duration")
+        minDurationEditText = view.findView<EditText>("minDuration")
+        maxDurationEditText = view.findView<EditText>("maxDuration")
+        addSearchHomeTextView = view.findView<TextView>("addSearchHome")
+        searchHomeEditText = view.findView<EditText>("searchHomeEdit")
+        searchHomeSortTextView = view.findView<TextView>("searchSort")
+        addSearchHomeSortEditText = view.findView<EditText>("addSearchSort")
+        searchSortHintTextView = view.findView<TextView>("searchSortHint")
+        disclaimerTextView = view.findView<TextView>("disclaimerText")
 
         // Set strings
         hdOnlySwitch.text = getString("hd_only")
         durationTextView.text = getString("duration")
         minDurationEditText.hint = getString("min_duration")
         maxDurationEditText.hint = getString("max_duration")
-        saveButton.text = getString("save_button")
         addSearchHomeTextView.text = getString("add_search_home")
         searchHomeEditText.hint = getString("add_search_home_hint")
         searchHomeSortTextView.text = getString("search_sorting")
         addSearchHomeSortEditText.hint = getString("search_sorting_hint")
-        clearButton.text = getString("clear_button")
         searchSortHintTextView.text = getString("search_sorting_all")
         disclaimerTextView.text = getString("disclaimer")
 
@@ -100,8 +115,24 @@ class NSFWFiltersFragment(
             clearFilters()
         }
 
-        // Handle save_button button
-        saveButton.setOnClickListener {
+        // Handle clear_button
+//        clearButton.setOnClickListener {
+//            clearFilters()
+//        }
+    }
+
+    private fun clearFilters() {
+        sharedPref.edit().apply {
+            clear()
+            commit()
+        }
+        showToast(getString("cleared_toast"))
+        cleared = true
+        dismiss()
+    }
+
+    override fun onDestroy() {
+        if (!cleared) {
             sharedPref.edit().apply {
                 putBoolean(HD_ONLY, hdOnlySwitch.isChecked)
                 putInt(MIN_DURATION, minDurationEditText.text.toString().trim().toIntOrNull() ?: 0)
@@ -115,21 +146,8 @@ class NSFWFiltersFragment(
                 commit()
             }
             showToast(getString("saved_toast"))
-            dismiss()
         }
-
-        // Handle clear_button
-        clearButton.setOnClickListener {
-            clearFilters()
-        }
-    }
-
-    private fun clearFilters() {
-        sharedPref.edit().apply {
-            clear()
-            commit()
-        }
-        showToast(getString("cleared_toast"))
-        dismiss()
+        cleared = false
+        super.onDestroy()
     }
 }
